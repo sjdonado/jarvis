440528ab8570fa48fa16ce1145ee7e84bcaaf4c6
Juan Rodriguez <sjdonado@icloud.com>
2024-10-26 13:18:33 +0200

    refactor: replace server with remix app with express

--- client/main.c
+++ client/main.c
@@ -78,7 +78,7 @@ void update_statusbar(const char *status_text) {
 
   const int statusbar_y = 0;
   const int statusbar_x = 0;
-  sFONT *font = &Font8;
+  sFONT *font = &Font12;
 
   // Calculate text width for right alignment
   int text_width = strlen(status_text) * font->Width;
